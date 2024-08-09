2e1fddcb3930aeb9c9140a753775a356f9cc900c
Rajdeep Debnath <rajdeepdebnath522@gmail.com>
2024-08-09 19:24:08 +0530

    Diamond shape pattern

---
+++ diamondPattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number"
+    return 0;
+}
+
+//      *
+//    * * * 
+//  * * * * *
+//* * * * * * *
+//* * * * * * * 
+//  * * * * * 
+//    * * * 
+//      *
