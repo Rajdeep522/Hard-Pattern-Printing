1cfcfd64a6dcc8a2b077155ac3901052fb3086ec
Rajdeep Debnath <rajdeepdebnath522@gmail.com>
2024-08-12 07:47:52 +0530

    Questions

---
+++ hollowdiamodPattern.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n); 
+    int rows, columns;
+    for (rows = 1; rows <= n; rows++) {
+        for (columns = n; columns > rows; columns--) {
+            printf(" ");
+        }
+        printf("*");
+        for (columns = 1; columns < (rows - 1) * 2; columns++) {
+            printf(" ");
+        }
+        if (rows != 1) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (rows = n - 1; rows >= 1; rows--) {
+        for (columns = n; columns > rows; columns--) {
+            printf(" ");
+        }
+        printf("*");
+        for (columns = 1; columns < (rows - 1) * 2; columns++) {
+            printf(" ");
+        }
+        if (rows != 1) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+// Enter a number: 5
+// 
+// 
+//        *
+//       * *
+//      *   *
+//     *     *
+//    *       *
+//     *     *
+//      *   *
+//       * *
+//        *
+// 
+// 
+ 
