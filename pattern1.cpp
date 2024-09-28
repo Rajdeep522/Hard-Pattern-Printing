4586d4e794a06b0edf30e3704091246e1145a00f
Rajdeep Debnath <158467163+Rajdeep522@users.noreply.github.com>
2024-09-28 22:25:10 +0530

    Update pattern1.cpp

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    
-    return 0;
+    int n;
+    cin>>n;
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n-i; j++){
+            cout<<"  ";
+        }
+        for (int j = i; j >=1; j--){
+            cout<<j<<" ";
+        }
+        for (int k = 2; k <=i ; k++){
+            cout<<k<<" ";
+        }
+        cout<<endl;
+    }
+ return 0;
 }
+
+// for n=5
+//
+//        1
+//      2 1 2
+//    3 2 1 2 3
+//  4 3 2 1 2 3 4
+//5 4 3 2 1 2 3 4 5
